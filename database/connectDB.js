f32cd48a28f843eaab697b74925420679755de30
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2024-03-16 11:23:53 -0300

    commi

--- database/connectDB.js
+++ database/connectDB.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 try {
-  await mongoose.connect(process.env.DB_URI);
+  await mongoose.connect(process.env.MONGO_URI);
   console.log("Database connected succesfully 🙌🙌🙌");
 } catch (error) {
   console.log("Something is wrong with the Database 😒😒😒");
